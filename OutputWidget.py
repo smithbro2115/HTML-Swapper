19aef8d557d7885770d009e997c4567fa882b5dc
Joshua <smithbro2115@gmail.com>
2019-02-04 20:36:25 -0600

    - Added separate OutputWidget file

---
+++ OutputWidget.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'D:\Programming\youtube_embed_to_link\OutputWidget.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(292, 62)
+        self.gridLayout_2 = QtWidgets.QGridLayout(Form)
+        self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.frame = QtWidgets.QFrame(Form)
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Plain)
+        self.frame.setObjectName("frame")
+        self.gridLayout = QtWidgets.QGridLayout(self.frame)
+        self.gridLayout.setObjectName("gridLayout")
+        self.pushButton = QtWidgets.QPushButton(self.frame)
+        self.pushButton.setObjectName("pushButton")
+        self.gridLayout.addWidget(self.pushButton, 0, 1, 1, 1)
+        self.groupLabel = QtWidgets.QLabel(self.frame)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.groupLabel.setFont(font)
+        self.groupLabel.setObjectName("groupLabel")
+        self.gridLayout.addWidget(self.groupLabel, 0, 0, 1, 1)
+        self.replaceWithLabel = QtWidgets.QLabel(self.frame)
+        self.replaceWithLabel.setText("")
+        self.replaceWithLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.replaceWithLabel.setObjectName("replaceWithLabel")
+        self.gridLayout.addWidget(self.replaceWithLabel, 1, 0, 1, 2)
+        self.gridLayout.setColumnStretch(0, 1)
+        self.gridLayout.setColumnStretch(1, 3)
+        self.gridLayout_2.addWidget(self.frame, 1, 0, 1, 1)
+        self.gridLayout_2.setColumnStretch(0, 1)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "Edit"))
+        self.groupLabel.setText(_translate("Form", "Group 0"))
+
